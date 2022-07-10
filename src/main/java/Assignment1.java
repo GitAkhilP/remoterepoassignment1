1b51ebccc1deffb853377b075f767e19da16f878
Akhil Pandya <04immured.mats@icloud.com>
2022-07-09 22:12:40 -0400

    The first project in remote repo assignment

---
+++ src/main/java/Assignment1.java
@@ -0,0 +1,9 @@
+public class Assignment1 {
+    public static void main(String[] args){
+        int count = 0;
+        while (count != 5){
+            System.out.println("I already know java!");
+            count ++;
+        }
+    }
+}
